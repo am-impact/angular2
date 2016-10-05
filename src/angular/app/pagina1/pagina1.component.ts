985ad5d87eaab4385a85454b82a58e8a7f84ed9d
Niek Weevers <n.weevers@am-impact.nl>
2016-10-05 10:54:37 +0200

    Interpolation hoeven we niet meer te gebruiken ivm geen twig meer

--- src/angular/app/pagina1/pagina1.component.ts
+++ src/angular/app/pagina1/pagina1.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
-  templateUrl: './pagina1.component.html',
-  styleUrls: []
+  templateUrl: './pagina1.component.html'
 })
 export class Pagina1Component implements OnInit {
 
--- src/angular/app/pagina2/pagina2.component.ts
+++ src/angular/app/pagina2/pagina2.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagina2',
-  templateUrl: './pagina2.component.html',
-  styleUrls: []
+  templateUrl: './pagina2.component.html'
 })
 export class Pagina2Component implements OnInit {
 
